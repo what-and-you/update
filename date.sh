b99e8b2627325a49b9e1f3930564ac7386243744
ewepaksa123 <sejatipria990@gmail.com>
2024-06-03 16:08:54 +0700

    Update date.sh

--- date.sh
+++ date.sh
@@ -1,6 +1,9 @@
 clear
 echo "TOOLSRENV2 SEDANG DALAM PERBAIKAN/UPDATE"
 sleep 5
-echo "tekan enter untuk keluar"
+echo "tekan enter untuk keluar dan kalo ingin bertanya silakan ketik help"
+echo -p"help"
+xdg-open "https://wa.me/+6289519450908?text=bang ToolsRenv2 Kapan selesai update nya?"
 read -p" "
+
 exit
